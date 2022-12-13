3bf5fea9e2011f268e47ce81afd9d1455b0e4d84
NoorAlBattashi <nooralbattashi97@gmail.com>
2022-12-13 10:52:09 +0400

    Utility file

---
+++ src/Utility.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author LAP-9
+ *
+ */
+public class Utility {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
