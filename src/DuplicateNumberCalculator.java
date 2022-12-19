db0db3503b5c3a53f4781ffc8f2b3b8578cb5068
NoorAlBattashi <nooralbattashi97@gmail.com>
2022-12-19 21:24:49 +0400

    git duplication using loop and hashMap

--- src/DuplicateNumberCalculator.java
+++ src/DuplicateNumberCalculator.java
@@ -1,3 +1,4 @@
+
 /**
  * 
  */
@@ -6,28 +7,66 @@
  * @author LAP-9
  *
  */
+import java.util.HashMap;
+
 public class DuplicateNumberCalculator {
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int[] duplicatArr = { 0, 12, 8, 1, 12, 6, 8, 2, 6, 11, 1, 1 };
 
-		int[] dublicateNumber = { 1, 2, 3, 1, 2, 1 };
-		for (int number : dublicateNumber) {
-			int x = number;
-			int sum = 0;
-			
-			for (int dublicate : dublicateNumber) {
-				if (number == dublicate) {
-					sum++;
+		// numberOfDuplication using loop
+		int index = 0;
+		int element = 0;
+		boolean isExist = false;
+		for (int a = 0; a < duplicatArr.length; a++) {
+			int count = 0;
+			for (int b = a; b < duplicatArr.length; b++) {
+
+				if (duplicatArr[a] == duplicatArr[b]) {
+					count++;
+					index = a;
+					element = duplicatArr[a];
 				}
-				System.out.println(x + "repeated=" + sum + " Times");
+			}
+			for (int c = index - 1; c >= 0 && isExist == false; c--) {
+				if (element == duplicatArr[c]) {
+					isExist = true;
+				} else {
+					isExist = false;
+				}
+			}
+
+			if (isExist == false) {
+				System.out.println(element + " repeted " + count + " time(s)");
+			}
+			isExist = false;
+		}
+
+		// numberOfDuplication using HashMap
+		
+		// Array{ 0, 12, 8, 1, 12, 6, 8, 2, 6, 11, 1, 1 }
+		System.out.println("---------------------------------");
+
+		HashMap<Integer, Integer> duplicatNumHashMap = new HashMap<Integer, Integer>();
+        
+		//declare the hashMap objects
+		for (int declare = 0; declare < duplicatArr.length; declare++) {
+			duplicatNumHashMap.put(duplicatArr[declare], 0);
+		}
+        //calculate the duplication
+		for (int a = 0; a < duplicatArr.length; a++) {
+
+			if (duplicatNumHashMap.containsKey(duplicatArr[a])) {
+
+				duplicatNumHashMap.put(duplicatArr[a], duplicatNumHashMap.get(duplicatArr[a]) + 1);
 			}
 
 		}
+		System.out.println(duplicatNumHashMap);
 
-	}
+	}// end
 
 }
--- src/HashMapWorld.java
+++ src/HashMapWorld.java
@@ -20,16 +20,16 @@ public class HashMapWorld {
 		studentInfo.put("id", "h794rh");
 		studentInfo.put("age", "789");
 
-		// System.out.println(studentInfo.get("age"));
+		System.out.println(studentInfo.get("age"));
 		// studentInfo.clear();
-
-		for (String currentKey : studentInfo.keySet()) {
-			System.out.println(currentKey);
-		}
-		System.out.println("--------------");
-		for (String currentKey : studentInfo.values()) {
-			System.out.println(currentKey);
-		}
+//
+//		for (String currentKey : studentInfo.keySet()) {
+//			System.out.println(currentKey);
+//		}
+//		System.out.println("--------------");
+//		for (String currentKey : studentInfo.values()) {
+//			System.out.println(currentKey);
+//		}
 
 	}
 
