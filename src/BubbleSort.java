32abb3e3d113f3c3ae0c39e24ac84f42b5eb57c3
NoorAlBattashi <nooralbattashi97@gmail.com>
2022-12-20 11:05:09 +0400

    latest update

--- src/BubbleSort.java
+++ src/BubbleSort.java
@@ -11,42 +11,29 @@ public class BubbleSort {
 	/**
 	 * @param args
 	 */
-	private static int [] bubbleSort(int [] bubbleArr) {
-		int greater =0;
-		int greaterIndex =0;
-		int smallest =0;
-		int smallestIndex =0;
-		for(int a = 0 ; a < bubbleArr.length; a++) {
-			
-			for(int b = a+1; b< bubbleArr.length; b++) {
-				if(bubbleArr[a] > bubbleArr[b]) {
-					greater = bubbleArr[a];
-					greaterIndex = a;
-					smallest = bubbleArr[b];
-					smallestIndex = b;
-					bubbleArr[b]= smallest;
-					bubbleArr[a] = greater;
-				}else if(bubbleArr[a] < bubbleArr[b]) {
-					greater = bubbleArr[b];
-					greaterIndex = b;
-					smallest = bubbleArr[a];
-					smallestIndex = a;
-					bubbleArr[b]=smallest ;
-					bubbleArr[a] = greater;
+	private static int[] bubbleSort(int[] bubbleArr) {
+		int greater = 0;
+
+		for (int a = 0; a < bubbleArr.length; a++) {
+
+			for (int b = 0; b < bubbleArr.length - 1; b++) {
+				if (bubbleArr[b] < bubbleArr[b + 1]) {
+					greater = bubbleArr[b + 1];
+					bubbleArr[b + 1] = bubbleArr[b];
+					bubbleArr[b] = greater;
 				}
 			}
-			
 		}
-		return bubbleArr;	
+		return bubbleArr;
 	}
+
 	public static void main(String[] args) {
-		int [] bubbleArr = {4,1,5,7,2};
-		int [] bubble2Arr = {1,2,3,4,5};
-		for(int element: bubbleSort(bubble2Arr)) {
-			System.out.println(element);
+		int[] bubbleArr = { 4, 1, 5, 7, 2 };
+		int[] bubble2Arr = { 1, 2, 3, 4, 5 };
+		for (int element : bubbleSort(bubble2Arr)) {
+			System.out.print(element + " ");
 		}
-		
-		
+
 	}
 
 }
